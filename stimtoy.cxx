03a7f30f1bab02a0924acefd231c1dac6ba97397
vesper <dezalk.kyakha@gmail.com>
2025-04-29 00:28:32 -0400

    create stimtoy with basic datatypes

--- stimtoy.cxx
+++ stimtoy.cxx
@@ -1,5 +1,15 @@
+#include <chrono>
 #include <iostream>
+#include "stimtoy.hxx"
+#include <thread>
+
+using namespace std::chrono_literals;
 
 int main() {
-    std::cout << "Hello World!\n";
+    stimtoy st;
+    do {
+        st.sallys_first_stim();
+        std::cout << st.u32[0] << '\n';
+        std::this_thread::sleep_for(100ms);
+    } while (st.u32[0] != 0);
 }
---
+++ stimtoy.hxx
@@ -0,0 +1,43 @@
+#ifndef STIMTOY
+#define STIMTOY
+
+#include <array>
+#include <bitset>
+
+// https://archiveofourown.org/works/58671715/chapters/167421601
+union stimtoy {
+    // boolean
+    std::bitset<8192> b;
+
+    // integer
+    std::array<uint8_t, 1024> u8;
+    std::array<uint16_t, 512> u16;
+    std::array<uint32_t, 256> u32;
+    std::array<uint64_t, 128> u64;
+    std::array<int8_t, 1024> i8;
+    std::array<int16_t, 512> i16;
+    std::array<int32_t, 256> i32;
+    std::array<int64_t, 128> i64;
+
+    // floating point
+    std::array<float, 256> fp32;
+    std::array<double, 128> fp64;
+
+    // character
+    std::array<char8_t, 1024> c8;
+    std::array<char16_t, 512> c16;
+    std::array<char32_t, 256> c32;
+
+
+    stimtoy() {
+        u8 = {};
+    }
+
+    // "I held the leading four bytes of the block of memory, interpreted it as a number, and repeatedly alternated between multiplying it by 1,664,525 and adding 101,3904,223 for a satisfying pseudorandom walk through the entire sequence."
+    void sallys_first_stim() {
+        u32[0] *= 1664525;
+        u32[0] += 1013904223;
+    }
+};
+
+#endif /* STIMTOY */
