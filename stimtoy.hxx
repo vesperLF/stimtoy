0777f405a889fc86739959fd611f5c9c21a7ec5b
vesper <dezalk.kyakha@gmail.com>
2025-04-29 00:51:06 -0400

    add xoshiro stim

--- stimtoy.hxx
+++ stimtoy.hxx
@@ -28,6 +28,9 @@ union stimtoy {
     std::array<char16_t, 512> c16;
     std::array<char32_t, 256> c32;
 
+    // xoshiro256++ state
+    std::array<std::array<uint64_t, 4>, 32> xoshiro_state;
+
 
     stimtoy() {
         u8 = {};
@@ -37,6 +40,29 @@ union stimtoy {
     void sallys_first_stim() {
         u32[0] *= 1664525;
         u32[0] += 1013904223;
+    }
+
+private:
+    static inline uint64_t rotl(uint64_t x, int k) {
+        return (x << k) | (x >> (64 - k));
+    }
+public:
+
+    // https://prng.di.unimi.it/
+    uint64_t xoshiro_stim(size_t i) {
+        std::array<uint64_t, 4>& s = xoshiro_state[i];
+        const uint64_t result = rotl(s[0] + s[3], 23) + s[0];
+
+        const uint64_t t = s[1] << 17;
+
+        s[2] ^= s[0];
+        s[3] ^= s[1];
+        s[1] ^= s[2];
+        s[0] ^= s[3];
+        s[2] ^= t;
+        s[3] = rotl(s[3], 45);
+
+        return result;
     }
 };
 
